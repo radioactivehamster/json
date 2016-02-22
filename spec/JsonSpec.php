9706dc14241651c3268d462b4ed2049088a6f9f4
James Benner <james.benner@gmail.com>
2016-02-21 20:55:54 -0500

    Added `it_should_parse_a_json_file()` test.

--- spec/JsonSpec.php
+++ spec/JsonSpec.php
@@ -4,11 +4,20 @@ namespace spec\RadHam;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use stdClass;
 
 class JsonSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('RadHam\Json');
+    }
+
+    function it_should_parse_a_json_file()
+    {
+        $control = (object) ['hello' => 'world'];
+        $file    = __DIR__ . '/data/hello-world.json';
+
+        $this::parseFile($file)->shouldBeLike($control);
     }
 }
--- src/Json.php
+++ src/Json.php
@@ -2,7 +2,16 @@
 
 namespace RadHam;
 
+use RuntimeException;
+use SplFileInfo;
+
 class Json
 {
+    public function parseFile($filename)
+    {
+        $file     = new SplFileInfo($filename);
+        $contents = file_get_contents($file->getPathname());
 
+        return json_decode($contents);
+    }
 }
