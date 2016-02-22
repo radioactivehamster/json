55931fcc4470d8b0c2818d9edc49c03747095f2d
James Benner <james.benner@gmail.com>
2016-02-21 23:45:36 -0500

    Renamed variable for clarity.

--- spec/JsonSpec.php
+++ spec/JsonSpec.php
@@ -22,8 +22,8 @@ class JsonSpec extends ObjectBehavior
 
     function it_should_throw_a_runtime_exception_if_an_invalid_filename_is_supplied_to_parse_a_json_file()
     {
-        $file = __DIR__ . '/data/not-a-real-json-file.json';
+        $invalidFile = __DIR__ . '/data/not-a-real-json-file.json';
 
-        $this->shouldThrow('\RuntimeException')->duringParseFile($file);
+        $this->shouldThrow('\RuntimeException')->duringParseFile($invalidFile);
     }
 }
